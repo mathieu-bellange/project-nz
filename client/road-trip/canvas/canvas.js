0c21c9df34ac1e94fa33f7fce4bf5076c7b9f87f
mbellange <mbellange@gmail.com>
2017-10-02 17:44:41 +0200

    US 23 - ajout centrage du road trip sur un point

--- client/road-trip/canvas/canvas.js
+++ client/road-trip/canvas/canvas.js
@@ -2,38 +2,67 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Raphael from 'raphael';
 
+import './canvas.css';
 import * as NorthIsland from '../north-island';
 
 export default class RoadTripCanvas extends React.Component {
-  pixelRatio = 1;
   canvasId = 'roadTrip-canvas';
 
   static propTypes = {
     canvasWidth: PropTypes.number.isRequired,
-    canvasHeight: PropTypes.number.isRequired
+    canvasHeight: PropTypes.number.isRequired,
+    canvasCenter: PropTypes.object.isRequired,
+    pixelRatio: PropTypes.number.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      canvasStyle: {}
+    };
+    this.defineCanvasStyle = this.defineCanvasStyle.bind(this);
+  }
+
   componentDidMount() {
+    // init Rapahael
     const rsr = Raphael(
       this.canvasId,
-      this.props.canvasWidth * this.pixelRatio,
-      this.props.canvasHeight * this.pixelRatio
+      this.props.canvasWidth * this.props.pixelRatio,
+      this.props.canvasHeight * this.props.pixelRatio
     );
     rsr.setViewBox(
       0,
       0,
-      this.props.canvasWidth * this.pixelRatio,
-      this.props.canvasHeight * this.pixelRatio
+      this.props.canvasWidth * this.props.pixelRatio,
+      this.props.canvasHeight * this.props.pixelRatio
     );
-    const niLayer = new NorthIsland.Layer(rsr, this.pixelRatio);
+    // drawing on the canvas
+    const niLayer = new NorthIsland.Layer(rsr, this.props.pixelRatio);
     niLayer.draw();
-    const rniLayer = new NorthIsland.RoadLayer(rsr, this.pixelRatio);
+    const rniLayer = new NorthIsland.RoadLayer(rsr, this.props.pixelRatio);
     rniLayer.draw();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.canvasCenter !== this.props.canvasCenter) {
+      this.defineCanvasStyle();
+    }
+  }
+
+  defineCanvasStyle() {
+    this.setState({
+      canvasStyle: {
+        left: this.props.canvasCenter.x,
+        top: this.props.canvasCenter.y
+      }
+    });
   }
 
   render() {
     return (
-        <div id={this.canvasId}>
+        <div id="viewport">
+          <div id={this.canvasId} style={this.state.canvasStyle}>
+          </div>
         </div>
     );
   }
--- client/road-trip/road-trip.js
+++ client/road-trip/road-trip.js
@@ -4,6 +4,63 @@ import RoadTripCanvas from './canvas';
 export default class RoadTrip extends React.Component {
   width = 1080;
   height = 1120;
+  pixelRatio = 4;
+
+  constructor(props) {
+    super(props);
+    let windowWidth = 0;
+    let windowHeight = 0;
+    if (typeof window !== 'undefined') {
+      windowWidth = window.innerWidth;
+      windowHeight = window.innerHeight;
+    }
+    this.state = {
+      windowWidth,
+      windowHeight,
+      pixelRatio: this.pixelRatio,
+      canvasCenter: {
+        x: 0,
+        y: 0
+      }
+    };
+    this.onResize = this.onResize.bind(this);
+    this.centerCanvas = this.centerCanvas.bind(this);
+  }
+
+  centerCanvas() {
+    const point = {
+      x: 708 * this.state.pixelRatio,
+      y: 502 * this.state.pixelRatio
+    };
+    const x = (0 - point.x) + (this.state.windowWidth / 2);
+    const y = (0 - point.y) + (this.state.windowHeight / 2);
+    this.setState({
+      canvasCenter: { x, y }
+    });
+  }
+
+  onResize() {
+    if (typeof window !== 'undefined') {
+      this.setState({
+        windowWidth: window.innerWidth,
+        windowHeight: window.innerHeight
+      });
+      this.centerCanvas();
+    }
+  }
+
+  componentDidMount() {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('resize', this.onResize, false);
+    }
+    this.centerCanvas();
+  }
+
+  componentWillUnmount() {
+    if (typeof window !== 'undefined') {
+      window.removeEventListener('resize', this.onResize);
+    }
+  }
 
   render() {
     return (
@@ -11,6 +68,8 @@ export default class RoadTrip extends React.Component {
         <RoadTripCanvas
           canvasWidth={this.width}
           canvasHeight={this.height}
+          canvasCenter={this.state.canvasCenter}
+          pixelRatio={this.state.pixelRatio}
         ></RoadTripCanvas>
       </main>
     );
