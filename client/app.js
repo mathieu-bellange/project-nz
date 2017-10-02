69f42340eedf0fe68acd286a915285d41cae506f
mbellange <mbellange@gmail.com>
2017-10-02 17:43:58 +0200

    refacto composant App

--- client/app.js
+++ client/app.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import './app.css';
 import Nav from './nav';
 import Home from './home';
 import RoadTrip from './road-trip';
 import About from './about';
 
+class AppWrapper extends React.Component {
+  render() {
+    return (
+      <div id="wrapper">
+        <Nav></Nav>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/road-trip" component={RoadTrip}/>
+        <Route path="/about" component={About}/>
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
   <Router>
-    <div id="wrapper">
-      <Nav></Nav>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/road-trip" component={RoadTrip}/>
-      <Route path="/about" component={About}/>
-    </div>
+    <AppWrapper></AppWrapper>
   </Router>,
   document.getElementById('container')
 );
