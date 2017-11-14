d0a0bc508e967ad744c6fe58f074752f147fed33
mbellange <mbellange@gmail.com>
2017-11-14 14:34:08 +0100

    update marker.js
    ajout fonction permettant de vérifier que le point en plus d'être sur la droite est compris entre le point d'entrée et de sortie

--- client/road-trip/tools/marker.js
+++ client/road-trip/tools/marker.js
@@ -1,4 +1,4 @@
-import { add, divide, subtract, multiply } from 'mathjs';
+import { add, divide, subtract, multiply, sign } from 'mathjs';
 
 import Coordinate from './coordinate';
 
@@ -7,6 +7,10 @@ export default class Marker {
   id;
   begin: {};
   end: {};
+  alpha;
+  k;
+  deltaX;
+  deltaY;
 
   constructor(id, bx, by, ex, ey) {
     this.id = id;
@@ -14,9 +18,10 @@ export default class Marker {
     this.end = new Coordinate(ex, ey);
     this.alpha = divide(subtract(by, ey), subtract(bx, ex));
     this.k = subtract(by, multiply(this.alpha, bx));
+    this.deltaX = bx - ex;
+    this.deltaY = by - ey;
   }
 
-  // BACKLOG ajouter un check que le point se trouve bien dans la zone x-y défini par la droite
   // TODO supprimer le notion de pixelRatio trello:#68
   /**
    * Méthode permettant de savoir si un point est présent sur la droite correspondante
@@ -29,6 +34,18 @@ export default class Marker {
   isOn(point, pixelRatio) {
     const ratio = pixelRatio || 1;
     const calculY = Math.trunc(add(multiply(this.alpha, point.x / ratio), this.k));
-    return Math.trunc(point.y / ratio) - 1 <= calculY && calculY <= Math.trunc(point.y / ratio) + 1;
+    return this.isBetween(point, pixelRatio) && Math.trunc(point.y / ratio) - 1 <= calculY && calculY <= Math.trunc(point.y / ratio) + 1;
+  }
+
+  // DONE ajouter un check que le point se trouve bien dans la zone x-y défini par la droite
+  isBetween(point, pixelRatio) {
+    const ratio = pixelRatio || 1;
+    const deltaX = this.begin.x - (point.x / ratio);
+    const deltaY = this.begin.y - (point.y / ratio);
+    const isSameSignX = sign(deltaX) === sign(this.deltaX) || sign(deltaX) === 0;
+    const isSameSignY = sign(deltaY) === sign(this.deltaY) || sign(deltaY) === 0;
+    const isShorterX = Math.abs(deltaX) <= Math.abs(this.deltaX);
+    const isShorterY = Math.abs(deltaY) <= Math.abs(this.deltaY);
+    return isSameSignX && isSameSignY && isShorterX && isShorterY;
   }
 }
