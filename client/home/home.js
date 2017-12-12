bf75eaf42cce997110152b0a16b1e56f05569cfd
mbellange <mbellange@gmail.com>
2017-12-12 14:53:04 +0100

    US 32 - home
    refacto des composants

--- client/home/home.js
+++ client/home/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
 
-// PLANNING revoir le positionnement des composants sous mobiles trello:#34
+// DONE revoir le positionnement des composants sous mobiles trello:#34
 // DONE revoir le positionnement de l'image de fond sous mobiles trello:#34
 export default class Home extends React.Component {
   render() {
@@ -11,6 +11,7 @@ export default class Home extends React.Component {
         <header>
           <h1>Notre voyage</h1>
           <p>Expliqué par des gens bien d'une super façon</p>
+          <p>Un second paragraphe éventuel</p>
         </header>
         <div id="content">
           <img src="images/home_1920.jpg"></img>
