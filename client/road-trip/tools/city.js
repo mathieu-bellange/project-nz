a165daad54ecb1c1e953a5531e8c6b2b440d448d
mbellange <mbellange@gmail.com>
2018-01-10 11:26:34 +0100

    US 65 - cities
    ajout de l'affichage du nom des villes et métropoles

--- client/road-trip/tools/city.js
+++ client/road-trip/tools/city.js
@@ -16,6 +16,11 @@ export default class City extends Coordinate {
         stroke: '#f3f3f3',
         fill: '#f3f3f3'
       });
+    paper.text(this.x + (0.5 * this.pixelRatio), this.y - (0.75 * this.pixelRatio), this.name)
+      .attr({
+        'text-anchor': 'start',
+        'font-size': '16'
+      });
     return this;
   }
 }
--- client/road-trip/tools/metropolis.js
+++ client/road-trip/tools/metropolis.js
@@ -1,19 +1,26 @@
 import Path from './path';
+import Coordinate from './coordinate';
 
 export default class Metropolis extends Path {
   name;
+  namePosition;
 
-  constructor(coordinates, pixelRatio, name) {
+  constructor(coordinates, pixelRatio, name, x, y) {
     super(coordinates, {
       stroke: '#f3f3f3',
       fill: '#f3f3f3'
     });
     this.name = name;
+    this.namePosition = new Coordinate(x, y, pixelRatio);
     this.pixelRatio = pixelRatio;
   }
 
   draw(paper) {
     super.draw(paper, this.pixelRatio);
+    paper.text(this.namePosition.x, this.namePosition.y, this.name)
+      .attr({
+        'font-size': '32'
+      });
     return this;
   }
 }
