dd66d7ea550ef40d59675b826382ddcedee7f016
mbellange <mbellange@gmail.com>
2018-01-17 16:26:29 +0100

    refacto popin-text
    style

--- client/road-trip/popin/text-wrapper.js
+++ client/road-trip/popin/text-wrapper.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './text-wrapper.css';
+
 export default class ImgWrapper extends React.Component {
+  txtContainerElem;
+  txtWrapperElem;
+
   static propTypes = {
     box: PropTypes.object.isRequired
   };
 
+  constructor(props) {
+    super(props);
+    this.createMarkup = this.createMarkup.bind(this);
+  }
+
+  createMarkup() {
+    return { __html: this.props.box.text };
+  }
 
   render() {
     return (
       <div id={this.props.box.id} className="text-wrapper">
-        <h2>{this.props.box.title}</h2>
-        <p>{this.props.box.text}</p>
+        <div
+          className='text-container'
+          dangerouslySetInnerHTML={this.createMarkup()}
+        ></div>
+        <div className={`ellipsis`}>...</div>
       </div>
     );
   }
