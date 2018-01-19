d687c3ca4a105f898df9e59731451e63b0f6108e
mbellange <mbellange@gmail.com>
2018-01-19 15:17:26 +0100

    home
    ajout home.md

--- client/home/home.js
+++ client/home/home.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './home.css';
 
+import home from './home.md';
+
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.createMarkup = this.createMarkup.bind(this);
+  }
+
+  createMarkup() {
+    return { __html: home };
+  }
+
   render() {
     return (
       <main id="home">
-        <header>
-          <h1>Notre voyage</h1>
-          <p>Expliqué par des gens bien d'une super façon</p>
-          <p>Un second paragraphe éventuel</p>
-        </header>
+        <header dangerouslySetInnerHTML={this.createMarkup()}></header>
         <div id="content">
           <img src="images/home_1920.jpg"></img>
           <Link to="/road-trip">Commencer le voyage</Link>
---
+++ client/home/home.md
@@ -0,0 +1,5 @@
+# Notre voyage
+
+Expliqué par des gens bien d'une super façon
+
+Un second paragraphe éventuel
