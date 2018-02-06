659b4ede044191a9a37ea1e8afee58dfd55e3dfb
mbellange <mbellange@gmail.com>
2018-02-06 18:41:26 +0100

    kapiti-boat
    correctif svg

--- client/road-trip/scenery/kapiti-boat.js
+++ client/road-trip/scenery/kapiti-boat.js
@@ -1,25 +1,28 @@
 export default class KapitiBoat {
   svg;
+  boatPosition;
   boatSize = {
     w: 120,
     h: 62
   };
-  transformInstruction = '';
 
   draw(canvas, position, revert) {
     if (this.svg) this.svg.remove();
+    this.boatPosition = {
+      x: position.x - (this.boatSize.w / 2),
+      y: position.y - (this.boatSize.h / 2)
+    };
     let src = '/images/kapiti-boat.svg';
     if (revert) {
       src = '/images/kapiti-boat-reverse.svg';
     }
-    this.svg = canvas.image(src, position.x - (this.boatSize.w / 2), position.y - (this.boatSize.h / 2), this.boatSize.w, this.boatSize.h);
+    this.svg = canvas.image(src, this.boatSize.w, this.boatSize.h)
+      .move(this.boatPosition.x, this.boatPosition.y);
     return this;
   }
 
   animate() {
-    this.svg.animate({ transform: `${this.transformInstruction}t0,1` }, 100, () => {
-      this.svg.animate({ transform: `${this.transformInstruction}t0,0` }, 100, () => this.animate());
-    });
+    this.svg.animate(200, '-').move(this.boatPosition.x, this.boatPosition.y + 1).loop();
   }
 
   remove() {
