becccfcd135e7d51b0bd9f2758039544257ce708
mbellange <mbellange@gmail.com>
2018-09-13 17:06:30 +0200

    refactor scenario-service
    - suppression de la sauvegarde et de la suppression d'un scenario

--- client/road-trip/scenario/scenario.service.js
+++ client/road-trip/scenario/scenario.service.js
@@ -1,24 +1,12 @@
-// TODO refacto la class pour supprimer la notion de scenario, on ne conserve que l'étape trello:#126
+// DONE refacto la class pour supprimer la notion de scenario, on ne conserve que l'étape trello:#126
 export default class ScenarioService {
-  CURRENT_SCENARIO_TOKEN = 'currentScenario';
-
   CURRENT_STEP_TOKEN = 'currentStep';
-
-  saveCurrentScenario(indexScenario) {
-    localStorage.setItem(this.CURRENT_SCENARIO_TOKEN, indexScenario);
-  }
-
-  getCurrentScenario() {
-    const indexScenario = localStorage.getItem(this.CURRENT_SCENARIO_TOKEN) || 0;
-    return Number(indexScenario);
-  }
 
   saveCurrentStep(indexStep) {
     localStorage.setItem(this.CURRENT_STEP_TOKEN, indexStep);
   }
 
   getCurrentStep() {
-    const indexStep = localStorage.getItem(this.CURRENT_STEP_TOKEN) || 0;
-    return Number(indexStep);
+    return Number(localStorage.getItem(this.CURRENT_STEP_TOKEN) || 0);
   }
 }
