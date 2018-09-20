65cb3ef78b4d039e2eb0aa15ce61a0515e73dcfc
mbellange <mbellange@gmail.com>
2018-09-20 12:26:22 +0200

    request fullscreen
    - ajout du passage en full screen
    - changement d'icone sur le full screen

--- client/road-trip/full-screen/full-screen.js
+++ client/road-trip/full-screen/full-screen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { fromEvent } from 'rxjs';
 
 import './full-screen.sss';
 
@@ -8,9 +9,26 @@ export default class RoadController extends React.Component {
     loading: PropTypes.bool
   };
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullScreen: false
+    };
+    this.onFullScreenRequest = this.onFullScreenRequest.bind(this);
+  }
+
+  componentDidMount() {
+    fromEvent(window, 'webkitfullscreenchange')
+      .subscribe(() => this.setState({ fullScreen: !this.state.fullScreen }));
+  }
+
+  onFullScreenRequest() {
+     document.querySelector('body').webkitRequestFullscreen();
+  }
+
   render() {
     return (
-      <span className={`full-screen ${this.props.loading ? 'loading' : ''}`}>
+      <span className={`screen-full ${this.props.loading ? 'loading' : ''} ${this.state.fullScreen ? 'collapse' : ''}`} onClick={this.onFullScreenRequest}>
       </span>
     );
   }
