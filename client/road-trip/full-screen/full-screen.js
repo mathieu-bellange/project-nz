d262d37a504fcb7632362189b24be5174eb33120
mbellange <mbellange@gmail.com>
2018-09-20 16:15:27 +0200

    exit fullscreen
    - ajout de la fonction de sortie du mode pleine écran
    - refacto pour gérer ça avec rxjs

--- client/road-trip/full-screen/full-screen.js
+++ client/road-trip/full-screen/full-screen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { fromEvent } from 'rxjs';
+import { Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 import './full-screen.sss';
 
@@ -11,24 +12,23 @@ export default class RoadController extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      fullScreen: false
-    };
-    this.onFullScreenRequest = this.onFullScreenRequest.bind(this);
+    this.onFullScreenRequest = new Subject().pipe(filter(() => document.webkitFullscreenEnabled));
   }
 
   componentDidMount() {
-    fromEvent(window, 'webkitfullscreenchange')
-      .subscribe(() => this.setState({ fullScreen: !this.state.fullScreen }));
-  }
-
-  onFullScreenRequest() {
-     document.querySelector('body').webkitRequestFullscreen();
+    this.onFullScreenRequest
+      .pipe(filter(() => document.webkitFullscreenElement))
+      .subscribe(() => document.webkitExitFullscreen());
+    this.onFullScreenRequest
+      .pipe(filter(() => !document.webkitFullscreenElement))
+      .subscribe(() => document.querySelector('#wrapper').webkitRequestFullscreen());
   }
 
   render() {
     return (
-      <span className={`screen-full ${this.props.loading ? 'loading' : ''} ${this.state.fullScreen ? 'collapse' : ''}`} onClick={this.onFullScreenRequest}>
+      <span
+        className={`screen-full ${this.props.loading ? 'loading' : ''} ${document.webkitFullscreenElement ? 'collapse' : ''}`}
+        onClick={() => this.onFullScreenRequest.next()}>
       </span>
     );
   }
