6399c373113ff8db16babe7a4f93968d8ffc91d3
mbellange <mbellange@gmail.com>
2018-09-21 09:40:33 +0200

    compatibility firefox and edge
    - ajout des fonctions utilisable par edge et ff

--- client/road-trip/full-screen/full-screen.js
+++ client/road-trip/full-screen/full-screen.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Subject } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
 
 import './full-screen.sss';
 
 export default class RoadController extends React.Component {
+  fullscreenEnabled = document.fullscreenEnabled
+    || document.webkitFullscreenEnabled || document.mozFullScreenEnabled;
+
   static propTypes = {
     loading: PropTypes.bool
   };
 
   constructor(props) {
     super(props);
-    this.onFullScreenRequest = new Subject().pipe(filter(() => document.webkitFullscreenEnabled));
+    this.onFullScreenRequest = new Subject().pipe(filter(() => this.fullscreenEnabled));
   }
 
   componentDidMount() {
-    this.onFullScreenRequest
-      .pipe(filter(() => document.webkitFullscreenElement))
-      .subscribe(() => document.webkitExitFullscreen());
-    this.onFullScreenRequest
-      .pipe(filter(() => !document.webkitFullscreenElement))
-      .subscribe(() => document.querySelector('#wrapper').webkitRequestFullscreen());
+    this.onFullScreenRequest.pipe(
+      filter(() => document.fullscreenElement
+        || document.webkitFullscreenElement || document.mozFullScreenElement),
+      map(() => document.exitFullscreen || document.msExitFullscreen
+        || document.webkitExitFullscreen || document.mozCancelFullScreen)
+    ).subscribe(execute => execute.bind(document)());
+    this.onFullScreenRequest.pipe(
+      filter(() => !document.fullscreenElement
+        || !document.webkitFullscreenElement || !document.mozFullScreenElement),
+      map(() => document.querySelector('#wrapper')),
+      map(root => ({
+        root,
+        execute: root.requestFullscreen
+          || root.webkitRequestFullscreen || root.mozRequestFullScreen
+      }))
+    ).subscribe(o => o.execute.bind(o.root)());
   }
 
   render() {
     return (
       <span
-        className={`screen-full ${this.props.loading ? 'loading' : ''} ${document.webkitFullscreenElement ? 'collapse' : ''}`}
+        className={`screen-full ${this.props.loading ? 'loading' : ''} ${document.fullscreenElement
+          || document.webkitFullscreenElement || document.mozFullScreenElement ? 'collapse' : ''}`}
         onClick={() => this.onFullScreenRequest.next()}>
       </span>
     );
